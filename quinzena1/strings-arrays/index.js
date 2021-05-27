7e5b77a51a552a00368aa33e9f36f56b65562315
Anderson Leite <andersonheidrich@hotmail.com>
2021-05-27 11:07:15 -0300

    Strings-Arrays

--- quinzena1/strings-arrays/index.js
+++ quinzena1/strings-arrays/index.js
@@ -15,23 +15,23 @@ Valor impresso no console: "SUBI NUM ÔNIBUS EM MIRROCOS"
 // #######   EXERCÍCIOS DE ESCRITA DE CÓDIGO   #######
 
 // #######   EXERCÍCIO 1   #######
-
+/*
 const nome = prompt("Qual o seu nome?")
 const email = prompt("Qual o seu e-mail?")
 console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
-
+*/
 // #######   EXERCÍCIO 2   #######
 
 let comidas = ["pizza", "hamburger", "chocolate", "lasanha", "macarronada"]
+let comidas2 = comidas.toString()
+comidas2 = comidas2.replaceAll(",", "\n")
 console.log(comidas)
-comidas = String(comidas)
-comidas = comidas.replaceAll(",","\n")
-console.log("Essas são as minhas comidas preferidas: \n"+comidas)
+console.log("Essas são as minhas comidas preferidas: \n"+comidas2)
 const comidaPref = prompt("Qual sua comida preferida?")
-console.log("Nova lista: \n"+comidas.replaceAll("hamburger",comidaPref))
+console.log("Nova lista: \n"+comidas2.replaceAll("hamburger",comidaPref))
 
 // #######   EXERCÍCIO 3   #######
-
+/*
 let listaDeTarefas = []
 const tarefa1 = prompt("Digite a sua primeira tarefa.")
 const tarefa2 = prompt("Digite a sua segunda tarefa.")
@@ -41,3 +41,4 @@ console.log(listaDeTarefas)
 const indice = prompt("Digite o índice de uma tarefa que você já realizou: 0, 1 ou 2.")
 listaDeTarefas.splice(indice, 1)
 console.log(listaDeTarefas)
+*/
