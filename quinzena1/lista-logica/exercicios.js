e69ba520e0a4b5ea97f427549d6ae8c23384efb0
Anderson Leite <andersonheidrich@hotmail.com>
2021-05-28 11:41:43 -0300

    Projeto

--- quinzena1/lista-logica/exercicios.js
+++ quinzena1/lista-logica/exercicios.js
@@ -22,54 +22,58 @@ function imprimeMensagem() {
 
 // Exercício 1
 function calculaAreaRetangulo() {
-  let altura = Number(prompt("Digite a altura do retângulo em metros."))
-  let largura = Number(prompt("Digite a largura do retângulo em metros."))
+  const altura = Number(prompt("Digite a altura do retângulo em metros."))
+  const largura = Number(prompt("Digite a largura do retângulo em metros."))
 
   console.log(altura * largura)
 }
 
 // Exercício 2
 function imprimeIdade() {
-  let ano = Number(prompt("Digite o ano atual."))
-  let nascimento = Number(prompt("Digite o ano de seu nascimento."))
+  const anoAtual = Number(prompt("Digite o ano atual."))
+  const anoNascimento = Number(prompt("Digite o ano de seu nascimento."))
 
-  console.log(ano - nascimento)
+  console.log(anoAtual - anoNascimento)
 }
 
 // Exercício 3
 function calculaIMC() {
-  let peso = Number(prompt("Digite o seu peso em Kg."))
-  let altura = Number(prompt("Digite a sua altura em metros."))
-  let alturaAoQuadrado = altura * altura
-
-  console.log(peso / alturaAoQuadrado)
+  const peso = Number(prompt("Digite o seu peso em Kg."))
+  const altura = Number(prompt("Digite a sua altura em metros."))
+  
+  console.log(peso / (altura * altura))
 }
 
 // Exercício 4
 function imprimeInformacoesUsuario() {
-  let nome = prompt("Digite seu nome.")
-  let idade = Number(prompt("Digite a sua idade."))
-  let email = prompt("Digite o seu e-mail.")
+  const nome = prompt("Digite seu nome.")
+  const idade = Number(prompt("Digite a sua idade."))
+  const email = prompt("Digite o seu e-mail.")
 
   console.log("Meu nome é "+nome+", tenho "+idade+" anos, e o meu email é "+email+".")
 }
 
 // Exercício 5
 function imprimeTresCoresFavoritas() {
-  // TERMINAR
+  let cores = []
+  const cor1 = prompt("Digite sua primeira cor favorita.")
+  const cor2 = prompt("Digite sua segunda cor favorita.")
+  const cor3 = prompt("Digite sua terceira cor favorita.")
+  cores.push(cor1, cor2, cor3)
+  console.log(cores)
 }
 
 // Exercício 6
 function retornaStringEmMaiuscula() {
-  let palavra = prompt("Escreva uma palavra que contenha letras minúsculas.")
+  const palavra = prompt("Escreva uma palavra que contenha letras minúsculas.")
 
   console.log(palavra.toUpperCase())
 }
 
 // Exercício 7
 function calculaIngressosEspetaculo() {
-  let custo = Number(prompt("Digite o custo total do espetáculo."))
-  let ingresso = Number(prompt("Digite o preço de cada ingresso."))
+  const custo = Number(prompt("Digite o custo total do espetáculo."))
+  const ingresso = Number(prompt("Digite o preço de cada ingresso."))
 
   console.log(custo/ingresso)
 }
@@ -79,18 +83,16 @@ function calculaIngressosEspetaculo() {
 function checaStringsMesmoTamanho() {
   const palavra1 = prompt("Digite uma palavra.")
   const palavra2 = prompt("Digite outra palavra.")
-  const resultado = palavra1 > palavra2
-
-  console.log(resultado)
+  
+  console.log(palavra1 >= palavra2)
 }
 
 // Exercício 9
 function checaIgualdadeDesconsiderandoCase() {
-  let string1 = prompt("Digite uma palavra.")
-  let string2 = prompt("Digite outra palavra.")
-  let resultado = string1 !== string2
-
-  console.log(resultado)
+  const string1 = prompt("Digite uma palavra.")
+  const string2 = prompt("Digite outra palavra.")
+  
+  console.log(string1.toUpperCase() === string2.toUpperCase())
 }
 
 // Exercício 10
@@ -108,17 +110,16 @@ function checaRenovacaoRG() {
 
 // Exercício 11
 function checaAnoBissexto() {
-  let ano = Number(prompt("Digite um ano."))
-  bissexto = ((ano % 400) === 0) || ((ano % 4) === 0) && ((ano % 100) !== 0)
-  console.log(bissexto)
+  const ano = Number(prompt("Digite um ano."))
+  
+  console.log(((ano % 400) === 0) || ((ano % 4) === 0) && ((ano % 100) !== 0))
 }
 
 // Exercício 12
 function checaValidadeInscricaoLabenu() {
-  let idade = (prompt("Você tem mais de 18 anos?"))
-  let ensino = (prompt("Você possui ensino médio completo?"))
-  let dispon = (prompt("Você possui disponibilidade exclusiva durante os horários do curso?"))
+  const idade = (prompt("Você tem mais de 18 anos?"))
+  const ensino = (prompt("Você possui ensino médio completo?"))
+  const dispon = (prompt("Você possui disponibilidade exclusiva durante os horários do curso?"))
 
-  positivo = (idade === 'sim') && (ensino === idade) && (dispon === ensino)
-  console.log(positivo)
+  console.log((idade === 'sim') && (ensino === idade) && (dispon === ensino))
 }
